391d4518106f1bde284bf383664f64aa9870e7bb
LAPTOP-AMERC6E6\lenovo <1556116939@qq.com>
2019-09-28 14:35:47 +0800

    第二次作业

--- TheSecond/app/src/main/java/com/example/hello1/Hello1.java
+++ TheSecond/app/src/main/java/com/example/hello1/Hello1.java
@@ -11,7 +11,11 @@ import android.widget.Button;
 public class Hello1 extends AppCompatActivity implements View.OnClickListener {
 
     public static  final String TAG="Hello1";
-    public int count; //作为一个计数器
+    //ObjCount作为类的计数器是,对象公用的计数器
+    //mObjCount作为对象的计数器,对象私有的计数器
+    private static int ObjCount=0;
+    private int mObjCount;
+
 
 
     @Override
@@ -20,52 +24,43 @@ public class Hello1 extends AppCompatActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         settupClicks();
+        ObjCount++;
+        mObjCount=ObjCount;
         setTitle("Hello1");
-        //count=count+1;
-        //Log.d("Hello1"+"-"+count, "onCreate: ");
-
+        Log.d("Hello1"+"-"+mObjCount, "onCreate: ");
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-       // setTitle(TAG+"-"+count);
-       // Log.d(TAG+"-"+count ,"onStart: ");
-        Log.d(TAG, "onStart: ");
+        Log.d(TAG+"-"+mObjCount, "onStart: ");
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        //setTitle(TAG+"-"+count);
-        //Log.d(TAG+"-"+count , "onResume: ");
-        Log.d(TAG, "onResume: ");
+        Log.d(TAG+"-"+mObjCount, "onResume: ");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        //setTitle(TAG+"-"+count);
-        //Log.d(TAG+"-"+count , "onPause: ");
-        Log.d(TAG, "onPause: ");
+        Log.d(TAG+"-"+mObjCount, "onPause: ");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-       // setTitle(TAG+"-"+count);
-        //Log.d(TAG+"-"+count , "onStop: ");
-        Log.d(TAG, "onStop: ");
+        Log.d(TAG+"-"+mObjCount, "onStop: ");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        //setTitle(TAG+"-"+count);
-        //Log.d(TAG+"-"+count , "onDestroy: ");
-        Log.d(TAG, "onDestroy: ");
+        ObjCount--;
+        Log.d(TAG+"-"+mObjCount, "onDestroy: ");
     }
 
     @Override
@@ -75,8 +70,6 @@ public class Hello1 extends AppCompatActivity implements View.OnClickListener {
         if(v.getId()==R.id.btToHello1){
             intent=new Intent(Hello1.this,Hello1.class);
             startActivity(intent);
-            //Log.d(TAG+"-"+count, "onClick: ");
-            //setTitle(TAG+"-"+count);
         }
         if(v.getId()==R.id.btToHello2){
             intent=new Intent(Hello1.this,Hello2.class);
