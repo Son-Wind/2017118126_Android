f386c4f076b88562e900ee7b59f0b8567688ac43
cgs <1556116939@qq.com>
2019-11-16 23:08:37 +0800

    第五次作业_增删改

--- Recylerview/app/src/main/java/com/example/recylerview/Fruit.java
+++ Recylerview/app/src/main/java/com/example/recylerview/Fruit.java
@@ -11,6 +11,14 @@ public class Fruit {
         this.imageId = imageId;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
     public String getName() {
         return name;
     }
